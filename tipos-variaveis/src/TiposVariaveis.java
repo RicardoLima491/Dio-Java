89dbad5eb1d5d2f71753044260ba7b7540c4653f
Ricardo Lima <choco999@gmail.com>
2025-03-18 19:40:01 -0300

    update

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Tipos Primitivos
+
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 = (short) numeroNormal;
+       int numero = 1;
+       numero = 2;
+       final double VALOR_DE_PI = 3.14;
+       System.out.println (numero);
+       System.out.println(VALOR_DE_PI);
+
+       // String entre aspas
+
+
+       String meuNome = "Ricardo";
+       System.out.println(meuNome);
+
     }
 }
