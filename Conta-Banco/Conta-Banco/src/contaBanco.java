d05a86738ad0c518ed41b90e4a2d32592f941181
RicardoLima491 <ricardo.jose.lima@outlook.com>
2025-03-25 21:11:20 -0300

    Conhecendo os requisitos

--- Conta-Banco/Conta-Banco/src/contaBanco.java
+++
@@ -1,5 +0,0 @@
-public class contaBanco {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
---
+++ Conta-Banco/Conta-Banco/src/contaTerminal.java
@@ -0,0 +1,12 @@
+public class contaTerminal {
+    public static void main(String[] args) throws Exception {
+        System.out.println(" hahahahaha");
+        // TODO: Importar a classe Scanner
+
+        // Exibir as mensagens para o nosso usuario
+
+        // Obter pela Scanner os valores digitados no terminal
+
+        //Exibir a mensagem criada
+    }
+}
