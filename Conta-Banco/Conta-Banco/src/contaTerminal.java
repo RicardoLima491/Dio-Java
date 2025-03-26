8005fe68a5eb8d80c903ef35cc664111a075a24b
RicardoLima491 <ricardo.jose.lima@outlook.com>
2025-03-26 11:48:18 -0300

    Conta Terminal pronta para envio

--- Conta-Banco/Conta-Banco/src/contaTerminal.java
+++ Conta-Banco/Conta-Banco/src/contaTerminal.java
@@ -1,12 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println(" hahahahaha");
-        // TODO: Importar a classe Scanner
+  
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+       
+        System.out.println("Por favor, digite o número da Agência!");  //entrada de dados do usuario
+        String agencia = scanner.next();
+        System.out.println("Agora digite o número da sua conta");
+        int numeroConta = scanner.nextInt();
+        System.out.println("Digite seu nome");
+        String nomeCliente = scanner.next();
+       
+        System.out.println("Por favor digite seu saldo");
+        double saldo = scanner.nextDouble();
 
-        // Exibir as mensagens para o nosso usuario
+        System.out.println("Olá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta " + numeroConta +" e seu saldo de "+ saldo+" já está disponível para saque");
 
-        // Obter pela Scanner os valores digitados no terminal
-
-        //Exibir a mensagem criada
+       
     }
 }
