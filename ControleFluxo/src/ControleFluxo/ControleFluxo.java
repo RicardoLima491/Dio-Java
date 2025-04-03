cfa487e161893dfee141e24346ee3af8318839a2
RicardoLima491 <ricardo.jose.lima@outlook.com>
2025-04-03 13:43:15 -0300

    Controle de fluxo, fucionando!

--- ControleFluxo/src/ControleFluxo/ControleFluxo.java
+++ ControleFluxo/src/ControleFluxo/ControleFluxo.java
@@ -1,7 +1,7 @@
 package ControleFluxo;
 
-import java.util.Locale;
 import java.util.Scanner;
+
 public class ControleFluxo {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
@@ -10,14 +10,24 @@ public class ControleFluxo {
 		System.out.println("POR FAVOR INSIRA O SEGUNDO PARAMETRO: ");
 		int parametro2 = scanner.nextInt();
 		
-		System.out.println(parametro1+" "+ parametro2);
+		try {
+			contar(parametroUm,parametroDois);
 		
-		
-		
-		
-		
-		
+			
+		} catch (ParametrosInvalidosException e) {
+			System.out.println("Segundo parametro deve ser maior que o Primeiro");
+		}
 		
 	}
 
+	static int contar(int parametroUm , int parametroDois) throws ParametrosInvalidosException{
+		if (parametroDois -parametroUm <=0) {
+			throw new ParametrosInvalidosException();
+			
+		}else {
+			int contagem = parametroDois- parametroUm
+					for (contagem)
+			
+		}
+	}
 }
